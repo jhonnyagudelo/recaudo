02d0cb8a3d5978ff743479dbffcf00373e1323c5
jhonny stiven agudelo <jhonny.agudelo3120@gmail.com>
2019-01-23 07:41:29 -0500

    CORRECCION

---
+++ db/SQL/ejemplo_trigger.sql
@@ -0,0 +1,156 @@
+-- Function: im_detalle_1_actualiza_ingreso()
+
+-- DROP FUNCTION im_detalle_1_actualiza_ingreso();
+
+CREATE OR REPLACE FUNCTION im_detalle_1_actualiza_ingreso() RETURNS trigger AS
+$BODY$
+DECLARE
+  -- Declaramos las variables con el mismo tipo del definido en la tabla
+  iva_5 ingreso_mercaderia.im_impuesto_5%TYPE;
+  iva_10 ingreso_mercaderia.im_impuesto_10%TYPE;
+  curtime        timestamp;
+  fecha_compra   date;
+BEGIN
+  -- Esta es la forma correcta de obtener el "ahora"
+  -- segun la documentacion oficial
+  -- url: http://www.postgresql.org/docs/8.2/static/plpgsql-expressions.html
+  curtime := 'now';
+
+  -- Estamos insertando el registro?
+  IF TG_OP = 'INSERT' THEN
+    -- Obtenemos los totales de IVA 5 y 10%
+    SELECT
+      SUM(CASE WHEN imd_porc_impuesto = 5 THEN imd_impuesto ELSE 0 END) AS porc_5,
+      SUM(CASE WHEN imd_porc_impuesto = 10 THEN imd_impuesto ELSE 0 END) AS porc_10
+    INTO iva_5, iva_10
+    FROM im_detalle
+    WHERE idingmercaderia = NEW.idingmercaderia;
+
+    -- obtenemos; la fecha de compra de la tabla cabecera
+    SELECT im_fecha_compra
+    INTO fecha_compra
+    FROM ingreso_mercaderia
+    WHERE idingmercaderia = NEW.idingmercaderia;
+
+    -- actualizamos la tabla de arti­culo con la fecha de la nueva compra
+    UPDATE articulo
+    SET art_f_ultcompra = fecha_compra
+    WHERE idarticulo = NEW.idarticulo;
+
+    -- ahora actualizamos los estadi­sticos del ariÃ­culo
+    PERFORM fn_actualizar_estadisticos_articulo(NEW.idarticulo);
+
+    -- Actualizamos la tabla de ingreso de mercaderi­as sumando los totales
+    UPDATE ingreso_mercaderia
+    SET
+        im_subtotal = im_subtotal + NEW.imd_subtotal,
+        im_descuentos = im_descuentos + NEW.imd_descuento,
+        im_impuesto_5 = iva_5,
+        im_impuesto_10 = iva_10,
+        im_impuestos = iva_5 + iva_10,
+        im_total = im_total + NEW.imd_precio_final
+    WHERE idingmercaderia = NEW.idingmercaderia;
+
+    -- Actualizamos la tabla de arti­culo con el nuevo precio de compra (costo)
+    -- Si el precio de compra es 0 entonces no hacemos nada
+    -- INFO: hay un trigger sobre arti­culo que guarda un historico de variaciones de costo en historico_precios
+    IF NEW.imd_precio_compra <> 0 THEN
+      UPDATE articulo
+      SET art_costo = NEW.imd_precio_compra,
+          art_f_act = curtime,
+          art_modif_por = current_user
+      WHERE idarticulo = NEW.idarticulo;
+    END IF;
+
+    RETURN NEW;
+  END IF;
+
+  -- Estamos actualizando el registro?
+  IF TG_OP = 'UPDATE' THEN
+    -- Obtenemos los totales de IVA 5 y 10%
+    SELECT
+      SUM(CASE WHEN imd_porc_impuesto = 5 THEN imd_impuesto ELSE 0 END) AS porc_5,
+      SUM(CASE WHEN imd_porc_impuesto = 10 THEN imd_impuesto ELSE 0 END) AS porc_10
+    INTO iva_5, iva_10
+    FROM im_detalle
+    WHERE idingmercaderia = NEW.idingmercaderia;
+
+    -- obtenemos la fecha de compra de la tabla cabecera
+    SELECT im_fecha_compra
+    INTO fecha_compra
+    FROM ingreso_mercaderia
+    WHERE idingmercaderia = NEW.idingmercaderia;
+
+    -- actualizamos la tabla de arti­culo con la fecha de la nueva compra
+    UPDATE articulo
+    SET art_f_ultcompra = fecha_compra
+    WHERE idarticulo = NEW.idarticulo;
+
+    -- ahora actualizamos los estadi­sticos del arti­culo
+    PERFORM fn_actualizar_estadisticos_articulo(NEW.idarticulo);
+
+    -- Actualizamos la tabla de ingreso de mercaderi­as, primero restamos los totales
+    -- anteriores, luego sumamos los nuevos totales
+    UPDATE ingreso_mercaderia
+    SET
+        im_subtotal = (im_subtotal - OLD.imd_subtotal) + NEW.imd_subtotal,
+        im_descuentos = (im_descuentos - OLD.imd_descuento) + NEW.imd_descuento,
+        im_impuesto_5 = iva_5,
+        im_impuesto_10 = iva_10,
+        im_impuestos = iva_5 + iva_10,
+        im_total = (im_total - OLD.imd_precio_final) + NEW.imd_precio_final
+    WHERE idingmercaderia = NEW.idingmercaderia;
+
+    -- Actualizamos la tabla de arti­culo con el nuevo precio de compra (costo)
+    -- Si el precio de compra es 0 entonces no hacemos nada
+    -- INFO: hay un trigger sobre arti­culo que guarda un historico de variaciones de costo en historico_precios
+    IF NEW.imd_precio_compra <> 0 THEN
+      UPDATE articulo
+      SET art_costo = NEW.imd_precio_compra,
+          art_f_act = curtime,
+          art_modif_por = current_user
+      WHERE idarticulo = NEW.idarticulo;
+    END IF;
+
+    RETURN NEW;
+  END IF;
+
+  -- Estamos borrando el registro
+  IF TG_OP = 'DELETE' THEN
+    -- Es IVA 5%?
+    IF OLD.imd_porc_impuesto = 5 THEN
+      iva_5 := OLD.imd_impuesto;
+    ELSE
+      iva_5 := 0;
+    END IF;
+
+   -- Es IVA 10%?
+   IF OLD.imd_porc_impuesto = 10 THEN
+      iva_10 := OLD.imd_impuesto;
+    ELSE
+      iva_10 := 0;
+    END IF;
+
+    -- ahora actualizamos
+    -- re-calculamos los estadi­sticos para el arti­culo ya sin el
+    -- precio que estamos borrando ahora
+    PERFORM fn_actualizar_estadisticos_articulo(OLD.idarticulo);
+
+    -- Actualizamos la tabla de ingreso de mercaderi­as descontando los totales
+    UPDATE ingreso_mercaderia SET
+      im_subtotal = im_subtotal - OLD.imd_subtotal,
+      im_descuentos = im_descuentos - OLD.imd_descuento,
+      im_impuesto_5 = im_impuesto_5 - iva_5,
+      im_impuesto_10 = im_impuesto_10 - iva_10,
+      im_impuestos = im_impuestos  - OLD.imd_impuesto,
+      im_total = im_total - OLD.imd_precio_final
+    WHERE idingmercaderia = OLD.idingmercaderia;
+
+    RETURN OLD;
+  END IF;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION im_detalle_1_actualiza_ingreso()
+  OWNER TO everdaniel;
---
+++ db/SQL/tirilla_marcada..sql
@@ -0,0 +1,68 @@
+next_bus:=(SELECT vehiculo  FROM turno WHERE id_turno = 3+1);
+
+WITH turn(id_turno) AS (
+	VALUES(1)
+	)
+	,consulta AS (
+		SELECT
+		c.id_turno
+		,t.numero_turno
+		,tp.nombre_marcada
+		,t.hora_salida
+		,tp.tiempo_max
+		,tp.tiempo_marcada
+		,tp.numero_caida
+		,t.vehiculo
+
+		,SUM(CASE WHEN tp.numero_caida >=1 THEN tp.numero_caida ELSE 0 END)
+						OVER(
+							PARTITION BY tp.id_turno
+							) AS total_caida
+
+
+    ,CASE
+				WHEN tp.nombre_marcada = 'Albeiro'
+  					THEN tp.numero_caida * 10000
+ 				WHEN tp.nombre_marcada = 'La Y'
+ 						THEN tp.numero_caida * 10000
+				WHEN tp.numero_caida >=1
+					THEN numero_caida * 5000
+				ELSE 0
+				END AS cancelar
+
+		FROM turn c
+		INNER JOIN tiempo tp
+			ON tp.id_turno = c.id_turno
+			INNER JOIN turno t
+			ON t.id_turno = tp.id_turno
+		WHERE TRUE
+			ORDER BY tp.tiempo_max
+		)
+
+	SELECT
+	c.id_turno
+	,c.vehiculo
+	,c.numero_turno
+	,c.nombre_marcada
+	,c.hora_salida
+	,c.tiempo_max
+	,c.tiempo_marcada
+	,c.numero_caida
+	,c.total_caida
+	,cancelar
+	,SUM(cancelar)OVER( PARTITION BY total_caida ) AS total_cancelar
+	,vehiculo
+	FROM consulta c;
+
+
+
+
+
+
+
+
+
+maxi candelaria = 70.000
+buga 50.000;
+costa rica 70.0001
+
