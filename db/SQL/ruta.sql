769b1385b614a174aba70f6e8f155207808adc07
jhonny stiven agudelo <jhonny.agudelo3120@gmail.com>
2019-02-01 15:43:52 -0500

    consulta demas ruta

--- db/SQL/ruta.sql
+++ db/SQL/ruta.sql
@@ -1,5 +1,5 @@
 WITH turn(turno_id) AS (
-  VALUES()
+  VALUES(2)
   )
 ,consulta AS(
   SELECT
@@ -7,5 +7,43 @@ WITH turn(turno_id) AS (
     ,t.numero_turno
     ,tp.nombre_marcada
     ,t.hora_salida
-    ,
+    ,tp.tiempo_max
+    ,tp.tiempo_marcada
+    ,tp.numero_caida
+    ,t.vehiculo
+    ,SUM(CASE WHEN tp.numero_caida >=1 THEN tp.numero_caida ELSE 0 END)
+            OVER(
+              PARTITION BY tp.id_turno
+              ) AS total_caida
+
+    ,CASE
+        WHEN tp.nombre_marcada = 'Antonio Nariño'
+          THEN tp.numero_caida * 0
+        WHEN tp.nombre_marcada = 'Rio Cauca'
+          THEN tp.numero_caida * 0
+        WHEN tp.numero_caida >=1
+          THEN tp.numero_caida * 5000
+      ELSE 0 END
+      AS cancelar
+
+      FROM turn tn
+      INNER JOIN turno t
+        ON tn.turno_id = t.id_turno
+      INNER JOIN tiempo tp
+        ON tp.id_turno = t.id_turno
+      WHERE TRUE
+      ORDER BY tp.tiempo_max
   )
+  SELECT
+  c.id_turno
+  ,c.numero_turno
+  ,c.nombre_marcada
+  ,c.hora_salida
+  ,c.tiempo_max
+  ,c.tiempo_marcada
+  ,c.numero_caida
+  ,c.total_caida
+  ,cancelar
+  ,SUM(cancelar)OVER( PARTITION BY total_caida ) AS total_cancelar
+
+FROM consulta c;
