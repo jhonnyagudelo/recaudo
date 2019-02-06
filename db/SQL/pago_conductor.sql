939ffc1dbb3b267816b67ff60ab15e7da7512ccd
jhonny stiven agudelo <jhonny.agudelo3120@gmail.com>
2019-02-06 12:02:46 -0500

    costo turno ajustes1

--- db/SQL/pago_conductor.sql
+++ db/SQL/pago_conductor.sql
@@ -1,33 +1,35 @@
 WITH turn(turno_id) AS (
-  VALUES(1)
+  VALUES(5)
   )
   ,salario AS (
     SELECT
-    t_s.valor_pago
+    t_s.valor_salario
     ,t.id_ruta
     ,t.id_turno
+    ,t_s.salario_id
     ,ct.bea_neto_total
-    ,CASE
-     WHEN r.salario_id = t_s.salario_id
-        THEN ct.bea_neto_total * t_s.valor_pago
-      WHEN t.id_ruta = 3 OR t.id_ruta = 4  OR t.id_ruta = 15 OR t.id_ruta = 16
-        THEN r.salario_id
-      ELSE 0
+          ,CASE
+            WHEN t_s.valor_salario >= 1
+              THEN t_s.valor_salario
+              else ct.bea_neto_total * t_s.valor_salario
       END AS pago_conductor
+
       FROM turn tn
       INNER JOIN turno t
         ON tn.turno_id = t.id_turno
       INNER JOIN ruta r
-	ON t.id_ruta = r.id_ruta
+	      ON t.id_ruta = r.id_ruta
       INNER JOIN salario t_s
         ON r.salario_id = t_s.salario_id
-	INNER JOIN costo_turno ct
-	ON t.id_turno = ct.id_costo_turno
+	    INNER JOIN costo_turno ct
+	      ON t.id_turno = ct.id_turno
      WHERE TRUE
+     AND ct.vehiculo = t.vehiculo
       ORDER BY t.id_turno
     )
     SELECT
-    s.valor_pago
+    s.valor_salario
+    ,s.salario_id
     ,s.id_ruta
     ,s.id_turno
     ,s.bea_neto_total
